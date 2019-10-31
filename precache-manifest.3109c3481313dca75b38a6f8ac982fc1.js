85e6d7be46182cc673f5495fdb9788982c87efd6
GuWei <a0983828796@gmail.com>
2019-10-31 10:02:46 +0800

    Updates

---
+++ precache-manifest.3109c3481313dca75b38a6f8ac982fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a698cdd344cbc841db6b14b0ab1336d",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "f889f79a31aa0921b3ac",
+    "url": "/color-app/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "390dd5a28ab38654f4a3",
+    "url": "/color-app/static/css/main.60a49282.chunk.css"
+  },
+  {
+    "revision": "f889f79a31aa0921b3ac",
+    "url": "/color-app/static/js/2.b0c01e7e.chunk.js"
+  },
+  {
+    "revision": "390dd5a28ab38654f4a3",
+    "url": "/color-app/static/js/main.ce545f09.chunk.js"
+  },
+  {
+    "revision": "565e7d6cf42eaadeedd8",
+    "url": "/color-app/static/js/runtime-main.67b536fe.js"
+  }
+]);
