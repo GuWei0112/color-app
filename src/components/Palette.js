fe6f7b09cdc63224df3f377d18e4e02f8c1ce481
GuWei <a0983828796@gmail.com>
2019-10-31 13:51:21 +0800

    add paletteList

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import "rc-slider/assets/index.css";
 import "../css/Palette.css";
-import Navbar from './Navbar'
+import Navbar from "./Navbar";
 export default ({ Paltette }) => {
   const [level, setLevel] = useState(500);
-  const [format,setFormat] = useState('hex')
+  const [format, setFormat] = useState("hex");
   const colorBoxes = Paltette.colors[level].map(color => {
     return (
       <ColorBox background={color[format]} key={color.name} name={color.name} />
@@ -14,19 +14,21 @@ export default ({ Paltette }) => {
   const changeLevel = newLevel => {
     setLevel(newLevel);
   };
-  const changeFormat=(e)=>{
-    setFormat(e.target.value)
-  }
+  const changeFormat = e => {
+    setFormat(e.target.value);
+  };
   return (
     <div className="Palette">
-      <Navbar level={level} 
-      changeLevel={changeLevel} handleChange={changeFormat}/>
-      {/* NavBar */}
-      <div className="Palette-colors">
-        {/* ColorBox */}
-        {colorBoxes}
-      </div>
-      {/* Footer */}
+      <Navbar
+        level={level}
+        changeLevel={changeLevel}
+        handleChange={changeFormat}
+      />
+      <div className="Palette-colors">{colorBoxes}</div>
+      <footer className="Palette-footer">
+        {Paltette.paletteName}
+        <span className="emoji">{Paltette.emoji}</span>
+      </footer>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Paltette from './components/Palette'
-import seedColor from './components/seedColor'
-import {generatePalette}from'./components/colorHelper'
+import React from "react";
+import ReactDOM from "react-dom";
+import Paltette from "./components/Palette";
+import seedColor from "./components/seedColor";
+import { generatePalette } from "./components/colorHelper";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PaletteList from "./components/PaletteList";
 const App = () => {
-    return (
-        <div>
-            <Paltette Paltette={generatePalette(seedColor[4])}/>
-        </div>
-    )
-}
+  const findPaltette = id => {
+    return seedColor.find(paltette => {
+      return paltette.id === id;
+    });
+  };
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" render={() => <PaletteList palettes={seedColor}/>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Paltette
+              Paltette={generatePalette(
+                findPaltette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      {/*
+      <div>
+        <Paltette Paltette={generatePalette(seedColor[4])} />        
+      </div>
+    */}
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
